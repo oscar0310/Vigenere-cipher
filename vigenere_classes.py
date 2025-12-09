92c13b96a264c9511ce7b4f74ab865f3ffd5fd9a
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2025-12-09 19:38:13 +0100

    #3 Añadiendo la clase Encipher a vigenere_classes.py
    SE añade la clase para cifrar y descifrar los mensajes

--- vigenere_classes.py
+++ vigenere_classes.py
@@ -27,3 +27,28 @@ class Message:
     def __str__(self):  # mensaje como string
         return self.content
     
+class Encipher(Message):
+    
+    def __init__(self, text, key): #constructor de la clase Encipher recibe el texto y la clave
+        super().__init__(text)  #llamamos al contructor de Message para normalizar el texto
+        self.key = ''.join(self._normalize(key))  #normalizamos la clave usando el metodo heredado de Message y lo unios en un solo string por si tuviera varias palabras la clave
+        self.key_indices=[chNum[k] for k in self.key] #convertimos la clave a indices usando el diccionario chNum
+
+    def cipher(self, mode=True): #Método para cifrar o descifrar el mensaje, mode=True para cifrar, False para descifrar
+
+        textcod = [] #almacenamos el texto cifrado o descifrado
+        key_length = len(self.key_indices)  #longitud de la clave
+
+        for i, char in enumerate(self.content): #vamos pasando por cada caracter del mensaje
+            indice =chNum[char] #obtenemos el indice del caracter actual
+            valor_clave =self.key_indices[i % key_length] #obtenemos el valor de la clave correspondiente al caracter actual
+                                                          #lo hacemos ciclico para que si el mensaje es mas largo que la clave, volvamos a empezar desde el principio de la clave
+
+            if mode: #Comprobamos si estamos cifrando o descifrando
+                new_indice=(indice + valor_clave) % n #ciframos el caracter sumando el indice del caracter y el valor de la clave
+            else:
+                new_indice=(indice - valor_clave) % n #desciframos el caracter restando el valor de la clave al indice del caracter
+
+            textcod.append(numCh[new_indice]) #añadimos el nuevo indice a la lista
+        
+        return ''.join(textcod)  #unimos la lista en un string y lo devolvemos
